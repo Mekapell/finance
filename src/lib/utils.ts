6b981af532f52767467bed50c77c828a2e13a0b2
Mekapell <77895623+Mekapell@users.noreply.github.com>
2026-09-06 12:55:39 +0700

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,20 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatTHB(amount: number): string {
+  return new Intl.NumberFormat("th-TH", {
+    style: "currency",
+    currency: "THB",
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
+
+export function formatDateTH(date: string | Date): string {
+  return new Intl.DateTimeFormat("th-TH", {
+    dateStyle: "medium",
+  }).format(new Date(date));
+}
